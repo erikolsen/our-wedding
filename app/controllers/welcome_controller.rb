7c1d63b861f53c3eb1bf2c3b64402785f395c7c7
Erik Olsen <erik.s.olsen@gmail.com>
2016-03-06 13:44:39 -0600

    Move ceremony and reception to seperate pages

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,16 @@
 class WelcomeController < ApplicationController
+  helper_method :navbar
+
   def index
+  end
+
+  def reception
+  end
+
+  def ceremony
+  end
+
+  def navbar
+    params[:navbar] || 'topbar'
   end
 end
